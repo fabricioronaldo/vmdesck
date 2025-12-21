8e889e6df59f8271bb64ce23984b828733a01730
Fabricio Velasco <fabriciorvelasco@gmail.com>
2025-12-21 20:10:58 -0300

    plataforma

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from ttkbootstrap.constants import *
 import ttkbootstrap as tb
-
+from pregao import nova_tela
 
 root = tb.Window(themename='superhero')
-
+#Configuração da Janela Principal
 root.title('Van Mex Comercial')
-root.geometry('500x350')
-
+root.geometry('700x400')
+root.minsize(width=500, height=300)
 
 h1_title = tb.Label(text='Menu Principal', font=('Helvetica', 28), bootstyle='defout')
 h1_title.pack(pady=20)
 
-b1 = tb.Button(root, text='Pregão', bootstyle='SUCCESS')
+b1 = tb.Button(root, text='Pregão', bootstyle='SUCCESS', command=nova_tela)
 b1.pack(side=LEFT, padx=5, pady=10)
-
+  
 root.mainloop()
